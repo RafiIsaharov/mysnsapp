d09b1932973daa330f60ae290ded6acd42b5a99f
rafi isaharov <isaharov@gmail.com>
2018-04-08 05:02:49 +0300

    sns confirm

--- app.js
+++ app.js
@@ -6,15 +6,21 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var snsRouter = require('./routes/sns');
+var snsSubscriptionConfirmation = require('aws-sns-subscription-confirmation');
+ 
 
+ 
+// Fix the wrong request content-type as required by the body-parser
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(snsSubscriptionConfirmation.overrideContentType());
+app.use(bodyParser.json());
+app.use(snsSubscriptionConfirmation.snsConfirmHandler());
 
 app.use('/', indexRouter);
 app.use('/sns', snsRouter);
