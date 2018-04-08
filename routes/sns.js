5423736d71a90a43b2cd6db7f6af02fe7d7461e4
rafi isaharov <isaharov@gmail.com>
2018-04-08 05:22:27 +0300

    sns confirm 2

--- routes/sns.js
+++ routes/sns.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var snsMiddleware = require('express-sns-validator');
+var snsSubscriptionConfirmation = require('aws-sns-subscription-confirmation');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/', snsMiddleware(), (req, res, next) => {
+router.post('/', snsSubscriptionConfirmation.overrideContentType(),snsSubscriptionConfirmation.snsConfirmHandler(), (req, res, next) => {
   // do stuff with req.body
  res.send('respond with a resource');
 });
